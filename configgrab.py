55c65bdf9875ef3e74eb6989c9abed365bcc16bd
Andy Fox <andrewfx@me.com>
2020-12-29 15:06:37 +0000

    bug fix

--- configgrab.py
+++ configgrab.py
@@ -1,40 +1,40 @@
 from netmiko import ConnectHandler
 
 rtr1 = {
-    'device type': 'cisco_xe',
-    'ip': 'x.x.x.x',
+    'device_type': 'cisco_xe',
+    'ip': '10.10.20.175',
     'username': 'cisco',
     'password': 'cisco', 
 }
 
 rtr2 = {
-    'device type': 'cisco_xe',
-    'ip': 'x.x.x.x',
+    'device_type': 'cisco_xe',
+    'ip': '10.10.20.176',
     'username': 'cisco',
     'password': 'cisco', 
 }
 
 nx1 = {
-    'device type': 'cisco_nxos',
-    'ip': 'x.x.x.x',
+    'device_type': 'cisco_nxos',
+    'ip': '10.10.20.177',
     'username': 'cisco',
     'password': 'cisco', 
 }
 
 nx2 = {
-    'device type': 'cisco_nxos',
-    'ip': 'x.x.x.x',
+    'device_type': 'cisco_nxos',
+    'ip': '10.10.20.178',
     'username': 'cisco',
     'password': 'cisco', 
 }
 
 with open('audit_commands') as c:
     lines = c.read().splitlines()
-print(lines)
+print (lines)
 
 all_devices = [rtr1, rtr2, nx1, nx2]
 
-for device in all_devices
+for device in all_devices:
     net_connect = ConnectHandler(**device)
     output = net_connect.send_command(lines)
     print(output)
